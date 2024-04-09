4525267b72a88243ffae5fda9d151a692082579d
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-10 02:04:41 +0600

    adjusted minimum height of outlet to screen height

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,11 +1,17 @@
-import NavBar from '../pages/shared/navbar/NavBar';
-import { Outlet } from 'react-router-dom';
+import { ToastContainer } from "react-toastify";
+import Footer from "../pages/shared/footer/Footer";
+import NavBar from "../pages/shared/navbar/NavBar";
+import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
     <div>
       <NavBar></NavBar>
-      <Outlet></Outlet>
+      <ToastContainer></ToastContainer>
+      <div className="min-h-[calc(100vh-260px)]"><Outlet></Outlet></div>
+      <div>
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
