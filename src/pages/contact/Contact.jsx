07ccff62d0113a4c1aeb80715647f92460c39d70
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-10 02:00:12 +0600

    updated spacing and contact informations

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,14 @@
+import { Helmet } from "react-helmet-async";
+
 const Contact = () => {
   return (
     <section className="bg-base-200 pt-14 lg:pt-0 lg:min-h-[760px] min-h-screen lg:h-screen flex items-center bg-cool">
-      <div className="grid max-w-6xl grid-cols-1 mx-auto md:grid-cols-2 md:divide-x md:glass my-10">
+      <Helmet>
+        <title>DH | Contact Us</title>
+      </Helmet>
+      <div className="grid max-w-6xl grid-cols-1 mx-auto md:grid-cols-2 md:divide-x md:glass my-10 w-[90%]">
         <div className="p-10 self-center  glass lg:md-transparent md:h-full flex flex-col justify-center mb-6 md:mb-0">
-          <h1 className="text-4xl font-bold">Get in touch</h1>
+          <h1 className="text-4xl font-bold mb-4">Get in touch</h1>
           <p className="pt-2 pb-4">Fill in the form to start a conversation</p>
           <div className="space-y-4">
             <p className="flex items-center">
@@ -19,7 +24,7 @@ const Contact = () => {
                   clipRule="evenodd"
                 ></path>
               </svg>
-              <span>Fake address, 9999 City</span>
+              <span>KHA-2/5, Dhaka, Bangladesh</span>
             </p>
             <p className="flex items-center">
               <svg
@@ -30,7 +35,7 @@ const Contact = () => {
               >
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
               </svg>
-              <span>123456789</span>
+              <span>+8801733663373</span>
             </p>
             <p className="flex items-center">
               <svg
@@ -42,7 +47,7 @@ const Contact = () => {
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
               </svg>
-              <span>contact@business.com</span>
+              <span>sadiqur057@gmail.com</span>
             </p>
           </div>
         </div>
