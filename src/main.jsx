07844c2cdda78f031edd927cacacebeb06e4d378
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-10 02:07:49 +0600

    used healmet provider to provide custom title

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {
-  RouterProvider,
-} from "react-router-dom";
-import routes from './routes/Routes.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/Routes.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./providers/AuthProvider.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router= {routes}></RouterProvider>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <HelmetProvider>
+      <RouterProvider router={routes}></RouterProvider>
+      </HelmetProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
